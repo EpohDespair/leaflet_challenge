08b60a14cc2ae4701acc7d30d62d3a9478c9b5ab
Hope Diebold <epohdespair@gmail.com>
2022-02-25 19:45:03 -0500

    updated logic.js

---
+++ static/js/logic.js
@@ -0,0 +1,101 @@
+// Creating the map object
+var myMap = L.map("map", {
+    center: [0, -150],
+    zoom: 3
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+  
+  // Store the API query variables.
+  var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    
+  // Get the data with d3.
+  d3.json(baseURL).then(function(response) {
+  
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: getColor(feature.properties.mag),
+          color: "#000000",
+          radius: getRadius(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+      }
+      // set different color from magnitude
+        function getColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+          return "#ea2c2c";
+        case magnitude > 4:
+          return "#ea822c";
+        case magnitude > 3:
+          return "#ee9c00";
+        case magnitude > 2:
+          return "#eecc00";
+        case magnitude > 1:
+          return "#d4ee00";
+        default:
+          return "#98ee00";
+        }
+      }
+      // set radiuss from magnitude
+        function getRadius(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+    
+        return magnitude * 4;
+      }
+      
+      // GeoJSON layer
+      L.geoJson(data, {
+        // Maken cricles
+        pointToLayer: function(feature, latlng) {
+          return L.circleMarker(latlng);
+        },
+        // cirecle style
+        style: styleInfo,
+        // popup for each marker
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+      }).addTo(myMap);
+    
+      // an object legend
+      var legend = L.control({
+        position: "bottomright"
+      });
+    
+      // details for the legend
+      legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+    
+        var grades = [0, 1, 2, 3, 4, 5];
+        var colors = [
+          "#98ee00",
+          "#d4ee00",
+          "#eecc00",
+          "#ee9c00",
+          "#ea822c",
+          "#ea2c2c"
+        ];
+    
+        // Looping through
+        for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+      };
+
+    legend.addTo(myMap)
+  
+  });
+  
+ 
